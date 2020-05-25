83c329204cd827a5c886398aa39f6e33a523f91a
Frozen Feelings <kiprutohcollo@gmail.com>
2020-05-25 20:09:04 +0300

    created my first class picture model in models.py

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Picture(models.Model):
+    title = models.CharField(max_length =30)
+    image = models.ImageField()
+    posted_date= models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length =200)
+    location = models.ForeignKey('Location',on_delete=models.CASCADE)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.description
+    
+       
+    def save_images(self):
+        self.save()
+    
+    @classmethod
+    def search_by_category(cls, search_term):
+        '''
+        Method to filter images by category
+        '''
+        result = cls.objects.filter(category__category__icontains=search_term)
+        return result 
