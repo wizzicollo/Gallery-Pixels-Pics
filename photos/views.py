ed4cf8a6fbdc6ac0f0bff53f097187ccebd89ab9
Frozen Feelings <kiprutohcollo@gmail.com>
2020-05-25 19:46:16 +0300

    defined my home request in views.py in the photos app

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    images = Picture.objects.all()
+    return render (request, 'home.html', {"images":images})
